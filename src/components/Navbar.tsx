3ead73261a12295152da3cb27ae9dce1fcb1c7f7
Gatsu-7 <Gatsu-7@users.noreply.github.com>
2025-03-16 18:33:14 +0530

    changed logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { cn } from "@/lib/utils";
 import { Menu, X } from "lucide-react";
-import logo from "../../public/favicon.jpg";
+import logo from "../../public/favicon.png";
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
